976d14c7525a50bf13d5952ef2591e36c7704daa
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-10-25 17:14:27 +0200

    Initial commit.

---
+++ transcribe.py
@@ -0,0 +1,28 @@
+import json
+
+import typer
+import torch
+
+from transformers import pipeline
+
+# TODO: Check if accelerate is installed
+
+
+def main(file_name: str, device_id: str, transcript_path: str):
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        "openai/whisper-large-v2",
+        torch_dtype=torch.float16,
+        device=f"cuda:{device_id}",
+    )
+
+    pipe.model = pipe.model.to_bettertransformer()
+
+    outputs = pipe(file_name, chunk_length_s=30, batch_size=24, return_timestamps=True)
+
+    with open(transcript_path, "w") as fp:
+        json.dump(sample, fp)
+
+
+if __name__ == "__main__":
+    typer.run(main)
