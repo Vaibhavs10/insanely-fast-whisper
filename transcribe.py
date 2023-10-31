265ce2de2793afbd59416d16e16f18c85076a7a7
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-10-31 12:10:18 +0100

    formatting.

--- transcribe.py
+++ transcribe.py
@@ -11,7 +11,7 @@ from typing_extensions import Annotated
 
 def main(
     file_name: Annotated[str, typer.Argument()], 
-    model: Annotated[str, typer.Argument()] = "openai/whisper-large-v2", 
+    model_name: Annotated[str, typer.Argument()] = "openai/whisper-large-v2", 
     task: Annotated[str, typer.Argument()] = "transcribe", 
     language: Annotated[str, typer.Argument()] = "en", 
     device_id: Annotated[str, typer.Argument()], 
@@ -19,14 +19,22 @@ def main(
 
     pipe = pipeline(
         "automatic-speech-recognition",
-        model",
+        model=model_name,
         torch_dtype=torch.float16,
         device=f"cuda:{device_id}",
     )
 
     pipe.model = pipe.model.to_bettertransformer()
 
-    outputs = pipe(file_name, chunk_length_s=30, batch_size=24, generate_kwargs={"task": task, "language": language}, return_timestamps=True)
+    outputs = pipe(
+        file_name, 
+        chunk_length_s=30, 
+        batch_size=24, 
+        generate_kwargs={
+            "task": task, 
+            "language": language
+            }, 
+        return_timestamps=True)
 
     with open(transcript_path, "w") as fp:
         json.dump(outputs, fp)
