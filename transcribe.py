8531aad4932784eee1f62767eedd1b179f4fe968
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-10-31 16:59:04 +0100

    better help text.

--- transcribe.py
+++ transcribe.py
@@ -9,28 +9,28 @@ parser.add_argument(
     "--file_name",
     required=True,
     type=str,
-    help="Path to the audio file to be transcribed",
+    help="Path or URL to the audio file to be transcribed.",
 )
 parser.add_argument(
     "--device_id",
     required=False,
     default="0",
     type=str,
-    help='Device ID for your GPU (just pass the device ID number) (default: "0")',
+    help='Device ID for your GPU (just pass the device ID number). (default: "0")',
 )
 parser.add_argument(
     "--transcript_path",
     required=False,
     default="output.json",
     type=str,
-    help="Path to save transcription output (default: output.json)",
+    help="Path to save the transcription output. (default: output.json)",
 )
 parser.add_argument(
     "--model_name",
     required=False,
     default="openai/whisper-large-v2",
     type=str,
-    help="Name of the pretrained model/ checkpoint to use (default: openai/whisper-large-v2)",
+    help="Name of the pretrained model/ checkpoint to perform ASR. (default: openai/whisper-large-v2)",
 )
 parser.add_argument(
     "--task",
@@ -38,14 +38,21 @@ parser.add_argument(
     default="transcribe",
     type=str,
     choices=["transcribe", "translate"],
-    help="Task to perform (default: transcribe)",
+    help="Task to perform, transcribe or translate to another language. (default: transcribe)",
 )
 parser.add_argument(
     "--language",
     required=False,
     type=str,
     default="en",
-    help='Language of the input speech (default: "en" (English))',
+    help='Language of the input audio. (default: "en" (English))',
+)
+parser.add_argument(
+    "--batch_size",
+    required=False,
+    type=int,
+    default=24,
+    help='Number of parallel batches you want to compute. Reduce if you face OOMs. (default: 24)',
 )
 
 args = parser.parse_args()
@@ -62,7 +69,7 @@ pipe.model = pipe.model.to_bettertransformer()
 outputs = pipe(
     args.file_name,
     chunk_length_s=30,
-    batch_size=24,
+    batch_size=args.batch_size,
     generate_kwargs={"task": args.task, "language": args.language},
     return_timestamps=True,
 )
